1822a9ea098a623e4f630f0e66b812446545df75
nestorLicona0896 <nestor_licona@hotmail.com>
2022-06-24 18:49:31 -0600

    estructura base para la biblioteca del programa clientetienda

---
+++ Tienda/src/producto.h
@@ -0,0 +1,18 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+namespace tp3
+{
+
+ class Producto {
+
+
+    public:
+
+        Producto();
+
+ };
+
+}
+
+#endif
---
+++ Tienda/src/tienda.h
@@ -0,0 +1,18 @@
+#ifndef TIENDA_H
+#define TIENDA_H
+
+namespace tp3
+{
+
+ class Tienda {
+
+
+    public:
+
+        Tienda();
+
+ };
+
+}
+
+#endif
